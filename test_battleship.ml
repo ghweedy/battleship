5b5b05f773841da35f3933784a9fd6d6b159cf7c
gleb <156471209+ghweedy@users.noreply.github.com>
2025-05-06 06:03:45 +0200

    Add files via upload

---
+++ test_battleship.ml
@@ -0,0 +1,265 @@
+(** Tests automatisés pour toutes les fonctions du projet Battleship.
+@author TOTSKYI Hlib
+*)
+
+(** Test unitaire de la fonction all_listes *)
+let test_all_listes_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de all_listes - à compléter avec des assert.";
+  (* Exemple : let res = all_listes arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction all_shoot *)
+let test_all_shoot_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de all_shoot - à compléter avec des assert.";
+  (* Exemple : let res = all_shoot arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction all_sunk *)
+let test_all_sunk_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de all_sunk - à compléter avec des assert.";
+  (* Exemple : let res = all_sunk arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction appl *)
+let test_appl_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de appl - à compléter avec des assert.";
+  (* Exemple : let res = appl arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction auto_placing_ships *)
+let test_auto_placing_ships_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de auto_placing_ships - à compléter avec des assert.";
+  (* Exemple : let res = auto_placing_ships arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction battleship_game *)
+let test_battleship_game_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de battleship_game - à compléter avec des assert.";
+  (* Exemple : let res = battleship_game arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction can_place_ship *)
+let test_can_place_ship_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de can_place_ship - à compléter avec des assert.";
+  (* Exemple : let res = can_place_ship arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction cell_to_pixel *)
+let test_cell_to_pixel_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de cell_to_pixel - à compléter avec des assert.";
+  (* Exemple : let res = cell_to_pixel arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction check_cell *)
+let test_check_cell_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de check_cell - à compléter avec des assert.";
+  (* Exemple : let res = check_cell arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction check_cell_x *)
+let test_check_cell_x_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de check_cell_x - à compléter avec des assert.";
+  (* Exemple : let res = check_cell_x arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction check_sunk_ship *)
+let test_check_sunk_ship_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de check_sunk_ship - à compléter avec des assert.";
+  (* Exemple : let res = check_sunk_ship arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction color_cell *)
+let test_color_cell_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de color_cell - à compléter avec des assert.";
+  (* Exemple : let res = color_cell arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction computer_shoot *)
+let test_computer_shoot_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de computer_shoot - à compléter avec des assert.";
+  (* Exemple : let res = computer_shoot arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction display_grid *)
+let test_display_grid_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de display_grid - à compléter avec des assert.";
+  (* Exemple : let res = display_grid arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction display_grid_color *)
+let test_display_grid_color_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de display_grid_color - à compléter avec des assert.";
+  (* Exemple : let res = display_grid_color arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction display_message *)
+let test_display_message_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de display_message - à compléter avec des assert.";
+  (* Exemple : let res = display_message arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction draw_grid *)
+let test_draw_grid_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de draw_grid - à compléter avec des assert.";
+  (* Exemple : let res = draw_grid arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction exists *)
+let test_exists_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de exists - à compléter avec des assert.";
+  (* Exemple : let res = exists arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction find_ship *)
+let test_find_ship_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de find_ship - à compléter avec des assert.";
+  (* Exemple : let res = find_ship arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction init_battleship *)
+let test_init_battleship_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de init_battleship - à compléter avec des assert.";
+  (* Exemple : let res = init_battleship arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction init_grid *)
+let test_init_grid_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de init_grid - à compléter avec des assert.";
+  (* Exemple : let res = init_grid arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction init_params *)
+let test_init_params_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de init_params - à compléter avec des assert.";
+  (* Exemple : let res = init_params arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction manual_placing_ship_list *)
+let test_manual_placing_ship_list_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de manual_placing_ship_list - à compléter avec des assert.";
+  (* Exemple : let res = manual_placing_ship_list arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction place_ship_on_grid *)
+let test_place_ship_on_grid_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de place_ship_on_grid - à compléter avec des assert.";
+  (* Exemple : let res = place_ship_on_grid arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction player_shoot *)
+let test_player_shoot_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de player_shoot - à compléter avec des assert.";
+  (* Exemple : let res = player_shoot arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction player_turns *)
+let test_player_turns_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de player_turns - à compléter avec des assert.";
+  (* Exemple : let res = player_turns arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction positions_list *)
+let test_positions_list_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de positions_list - à compléter avec des assert.";
+  (* Exemple : let res = positions_list arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction read_mouse *)
+let test_read_mouse_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de read_mouse - à compléter avec des assert.";
+  (* Exemple : let res = read_mouse arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction set_cell *)
+let test_set_cell_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de set_cell - à compléter avec des assert.";
+  (* Exemple : let res = set_cell arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction ship_sunk *)
+let test_ship_sunk_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de ship_sunk - à compléter avec des assert.";
+  (* Exemple : let res = ship_sunk arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction sink_ship *)
+let test_sink_ship_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de sink_ship - à compléter avec des assert.";
+  (* Exemple : let res = sink_ship arg in assert (res = valeur_attendue) *)
+;;
+
+(** Test unitaire de la fonction update_grid *)
+let test_update_grid_basic () =
+  (* Entrée de test basique à adapter selon la fonction réelle *)
+  print_endline "Test de update_grid - à compléter avec des assert.";
+  (* Exemple : let res = update_grid arg in assert (res = valeur_attendue) *)
+;;
+
+
+(* Lancement de tous les tests *)
+let () =
+  test_all_listes_basic ();
+  test_all_shoot_basic ();
+  test_all_sunk_basic ();
+  test_appl_basic ();
+  test_auto_placing_ships_basic ();
+  test_battleship_game_basic ();
+  test_can_place_ship_basic ();
+  test_cell_to_pixel_basic ();
+  test_check_cell_basic ();
+  test_check_cell_x_basic ();
+  test_check_sunk_ship_basic ();
+  test_color_cell_basic ();
+  test_computer_shoot_basic ();
+  test_display_grid_basic ();
+  test_display_grid_color_basic ();
+  test_display_message_basic ();
+  test_draw_grid_basic ();
+  test_exists_basic ();
+  test_find_ship_basic ();
+  test_init_battleship_basic ();
+  test_init_grid_basic ();
+  test_init_params_basic ();
+  test_manual_placing_ship_list_basic ();
+  test_place_ship_on_grid_basic ();
+  test_player_shoot_basic ();
+  test_player_turns_basic ();
+  test_positions_list_basic ();
+  test_read_mouse_basic ();
+  test_set_cell_basic ();
+  test_ship_sunk_basic ();
+  test_sink_ship_basic ();
+  test_update_grid_basic ();
+  print_endline "Tous les tests de base sont passés.";
+;;
